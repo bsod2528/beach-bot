ff3bec923f3cbe079390c6027f4ae97ddb6597db
AV <87459537+OAOImav@users.noreply.github.com>
2021-09-15 04:43:27 +0530

    Main.py

--- Program Files/Bitch.py
+++ Program Files/Bitch.py
@@ -7,7 +7,6 @@ import asyncio
 from discord import activity
 from discord.enums import Status
 from discord.ext import commands
-import pathlib
 
 # bot constructor
 bot = commands.Bot(command_prefix = '.b', status = discord.Status.idle, activity = discord.Game(name = 'Just a Bitch ;)') )
@@ -17,16 +16,15 @@ Token = # your bot token [ CHECK DISCORD DEVELOPER PORTAL ]
 @bot.event
 async def on_ready():
     print('Bitch Im REAADDDYYY!!!')
-# THE PRINT GIVEN ABOVE PRINTS IN OUR CONSOLE THAT THE BOT IS UP AND GOOD TO RUN
+# the print given above prints it in the console when you run the bot
 
 # COGS SETUP
-rootdir = pathlib.Path(__file__).parent.resolve()
-for filename in os.listdir(f'{rootdir}cogs'):
+for filename in os.listdir('D:\AV\PC\Coding\Discord Bot\Beach\Program Files\cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-# Before giving the pathway, we make a separate folder called *cogs*. This is where all our cogs will be stored.
-# For our bot to recognize the cogs in our cogs folder, we have written code from lines 21 to 23
+# before giving the pathway, we make a separate folder called *cogs*. This is where all our cogs will be stored.
+# for our bot to recognize the cogs in our cogs folder, we have written code from lines 21 to 23
 # os.listdr --> it is the directory present in our OS ( OPERATING SYSTEM ).
 # We give out the directory for our *cogs* folder and paste it in line 21.
 # bot.load_extension --> this command will load all the cogs into usage and hence there will be no problem.
@@ -37,18 +35,18 @@ for filename in os.listdir(f'{rootdir}cogs'):
 
 # command in the main bot.py
 @bot.command
-async def bich(ctx):
-    await ctx.send('Ye, I know your a bich. \n'
+async def beach(ctx):
+    await ctx.send('Yeah, you I love the beach!\n'
                     '*heads out silently*')
 # ctx --> context
 # all it does is represents the context in which a command is being invoked under.
 # you always do await to give output and *ctx.send* --> sends the context to the place where you have invoked the command
 
 @bot.command
-async def bichy(ctx):
+async def sandy(ctx):
     async with ctx.typing():
         await asyncio.sleep(0.5)
-    await ctx.send('Stop being so bitchy EWWW!!!!')
+    await ctx.send('Stop being so beachy EWWW!!!!')
 # async with ctx.typing --> all it does is it asynchronises with the context being typed sent where the command is invoked
 # await asyncio.sleep(0.5) --> it waits for the time ( sleep ) as when typing we take some time to write down the words, this replicates it.
 
@@ -61,19 +59,19 @@ async def embed(ctx):
         description = 'You add descriptions for the embed here. You can add more content by\n'
                        'Using New Line',
         color = ctx.author.color)
-# This is how one creates an embed. More colors will be present in ReadMe, check it out!
+# this is how one creates an embed. More colors will be present in ReadMe, check it out!
 
     emb.add_field(
         name = 'This is the title for a field',
         value = 'Description for a field is given by value',
         inline = False)
-# Inline False is given, otherwise your embed will look bazinga boing! 
-# Its your own personal preference, I like embeds with inline as False, experiment and see what you get without the inline value given. You'll knwo what I mean.
-# No need to mention *Color* as these are only extra fields for your embed above.
+# inline False is given, otherwise your embed will look bazinga boing! 
+# its your own personal preference, I like embeds with inline as False, experiment and see what you get without the inline value given. You'll knwo what I mean.
+# no need to mention *Color* as these are only extra fields for your embed above.
 
     await ctx.send(embed = emb)
-# You send your embed with ctx. Note to point, I have given emb, you guys can add whatever you want to. emb is only a variable name for the embed on the whole.
+# you send your embed with ctx. Note to point, I have given emb, you guys can add whatever you want to. emb is only a variable name for the embed on the whole.
 
 
-# Make sure the bot logs in 
+# make sure the bot logs in 
 bot.run('Token')

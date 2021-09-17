5391a0471f4c3b02bf0cc4cb37a373f12da31be7
AV <87459537+OAOImav@users.noreply.github.com>
2021-09-17 07:34:55 +0530

    Added Emotes Properly this time

--- Program Files/cogs/emote.py
+++ Program Files/cogs/emote.py
@@ -2,6 +2,7 @@ import discord
 import os
 import random
 import asyncio
+import datetime
 from discord.ext import commands
 
 # you do not require to import any new module for sending emotes!.
@@ -19,11 +20,15 @@ class EmotePy(commands.Cog):
     # a command to give out emotes
     @commands.command(alias = ['et'])
     async def emotetest(self, ctx):
+        async with ctx.typing():
+            await asyncio.sleep(0.5)
         await ctx.send(f'Normal Static Emote -> <:frogworry:887502713976410165>')
     
     # another example, but this time its and animated emote
     @commands.command(alias = ['at'])
     async def aniemote(self, ctx):
+        async with ctx.typing():
+            await asyncio.sleep(0.5)
         await ctx.send(f'Animated Emote --> <a:pepehehe:877959535137026059>')
     
     # a simple embed which outputs a random emote in an embed.
@@ -38,8 +43,11 @@ class EmotePy(commands.Cog):
         emb = discord.Embed(
             title = 'Emote Embed',
             description = f'{random.choice(emote)}',
-            color = ctx.author.color
-        )
+            color = ctx.author.color)
+        emb.timestamp = datetime.datetime.now(datetime.timezone.utc)
+        async with ctx.typing():
+            await asyncio.sleep(0.5)
         await ctx.send(embed = emb  )
+
 def setup(bot):
     bot.add_cog(EmotePy(bot))

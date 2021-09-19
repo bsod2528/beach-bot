565f18e98bca55aa7256e721b68cc7c75250fe36
Zeus432 <msiddhu@outlook.com>
2021-09-19 11:56:01 +0530

    Add ping and about commands in Utility Cog

--- Program Files/cogs/Utility.py
+++ Program Files/cogs/Utility.py
@@ -5,8 +5,39 @@ import os
 
 
 class Utility(commands.Cog):
+    """ Some basic Utility Commands """
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name = "ping", help = "View the ping of the bot", brief = "Take a wild guess")
+    @commands.guild_only()
+    #This restricts the command to be run only in guilds, the opposite being only dms would require the `@commands.dm_only()` decorator
+    async def ping(self, ctx):
+        #Basic ping command which gives the bot latency
+        await ctx.reply(f"Pong!\nMy Ping is `{round(self.bot.latency*1000)} ms`")
+    
+    @commands.command(name = "about", help = "View some info about the bot", brief = "Get Bot Info", aliases = ['info', "botinfo"])
+    @commands.guild_only()
+    async def about(self, ctx):
+        dev = self.bot.get_user(750979369001811982)
+        contributers = [760823877034573864]
+        contributers = [self.bot.get_user(us) for us in contributers]
+        #Here we get the all the users who contributed
+        colour = ctx.me.colour if ctx.me.colour != "#000000" else discord.Colour.random()
+        #Set the colour to the bots top role colour or a random colour if it doesn't have any colour
+        embed = discord.Embed(title="About Beach Bot", description = f"Beach Bot is a public bot made to be a fun and simple template / guide on how to create an amazing DISCORD BOT using Python Language. Click the Button below to take a look at the source code repo. This bot and its source code repository is made and maintained by **[{dev}]({dev.avatar})**\n\u200b", colour = colour)
+        embed.set_thumbnail(url=ctx.me.avatar)
+        embed.add_field(name="Other Contributers:",value="\n".join([f"**[{user}]({user.avatar})**" for user in contributers]))
+        embed.add_field(name="Coded in:",value=f"**Language:** **[`python 3.8.5`](https://www.python.org/)**\n**Library:** **[`discord.py 2.0`](https://github.com/Rapptz/discord.py)**\nㅤㅤㅤㅤ\U00002514 Master Branch")
+        #Add info about what the bot is coded in
+
+        #Get some more additions stats about the Bot
+        embed.add_field(name="Statistics:",value=f"**Servers:** {len([g.id for g in self.bot.guilds])} servers\n**Users:** {len([g.id for g in self.bot.users])}", inline = False)
+        embed.add_field(name="On Discord Since:",value=f"<t:{round(ctx.me.created_at.timestamp())}:D>")
+        #we'll now add a link button to the source code repo
+        view = discord.ui.View()
+        view.add_item(discord.ui.Button(style=discord.ButtonStyle.link, url = 'https://github.com/BSOD2528/Beach-Bot', label = "Github Repo", emoji = "<:github:889018277815279676>"))
+        await ctx.send(embed = embed, view = view)
 
     @commands.command(aliases  = ['src'])
     async def source(self, ctx, *, command: str = None):
